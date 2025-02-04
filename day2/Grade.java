4be4b166b264d862f9792e3f7b533aa829f0ddfb
Shabrin <shabrinksk4@gmail.com>
2025-02-04 11:30:01 +0530

    practice conditional statement

---
+++ day2/Grade.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(system.in);
+        int a=Scanner.nextint();
+        if(a>=90&&a<100){
+            System.out.println("you grade is A");
+        }
+        else if(a>80&&b<70){
+        System.out.println("your grade is B");
+        }
+        else{}
+        System.out.println("you are failed");
+    }
+
+}
---
+++ day2/SimpleIf.java
@@ -0,0 +1,9 @@
+public class SimpleIf {
+
+    public static void main(String[] args) {
+        int a=5,b=20;
+        if(a<b){
+            System.out.println(b+" is greater");
+        }
+    }
+}
